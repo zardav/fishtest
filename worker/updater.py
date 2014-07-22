3825e66aa4416e534350331f403ffb55eea30238
David Zar <zardav@gmail.com>
2014-07-22 17:07:36 +0300

    Start preparing for py2exe compiling

--- worker/updater.py
+++ worker/updater.py
@@ -1,13 +1,19 @@
 from __future__ import absolute_import
+from __builtin__ import file
 
 import os
 import requests
 import shutil
 import sys
+import subprocess
 from zipfile import ZipFile
 from distutils.dir_util import copy_tree
 
-WORKER_URL = 'https://github.com/glinscott/fishtest/archive/master.zip'
+Py2Exe = False
+
+WORKER_URL = 'https://github.com/glinscott/fishtest/archive/master.zip' if not Py2Exe \
+else 'https://github.com/glinscott/fishtest/releases/download/v1.0/fishtest-win.zip' # url of compiled executable
+
 
 def restart(worker_dir):
   """Restarts the worker, using the same arguments"""
@@ -35,7 +41,19 @@ def update():
   prefix = os.path.commonprefix([n.filename for n in zip_file.infolist()])
   fishtest_src = os.path.join(update_dir, prefix)
   fishtest_dir = os.path.dirname(worker_dir) # fishtest_dir is assumed to be parent of worker_dir
-  copy_tree(fishtest_src, fishtest_dir)
-  shutil.rmtree(update_dir)
+  if not Py2Exe:
+    copy_tree(fishtest_src, fishtest_dir)
+    shutil.rmtree(update_dir)
+    restart(worker_dir)
+  else:
+    finisher = file("finishUpdate.bat", 'w')
+    lines = ['ping 127.0.0.1 -n 2 > nul',   # wait 2 seconds
+             'xcopy %s\\* %s\\* /E /Y' % (fishtest_src, fishtest_dir), # copy tree
+             'rmdir /s /q %s' % update_dir, # delete tree
+             'start worker.exe %s' % " ".join(sys.argv[:])] # restart
+    finisher.write('\n'.join(lines))
+    finisher.close()
+    subprocess.Popen("finishUpdate.bat")
+    sys.exit(0)
+    
 
-  restart(worker_dir)
